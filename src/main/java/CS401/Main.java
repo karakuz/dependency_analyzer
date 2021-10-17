ee163446e82e2cdca319b842aa4cc3f50389f7fb
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2021-10-17 17:06:36 +0300

    deleted blabla

--- src/main/java/CS401/Main.java
+++ src/main/java/CS401/Main.java
@@ -19,43 +19,5 @@ public class Main {
     public static void main(String[] args) {
         Expression expressionNode = StaticJavaParser.parseExpression("1 + 2");
 
-        // JavaParser has a minimal logging class that normally logs nothing.
-        // Let's ask it to write to standard out:
-        Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
-        
-        // SourceRoot is a tool that read and writes Java files from packages on a certain root directory.
-        // In this case the root directory is found by taking the root from the current Maven module,
-        // with src/main/resources appended.
-        SourceRoot sourceRoot = new SourceRoot(CodeGenerationUtils.mavenModuleRoot(Main.class).resolve("src/main/resources"));
-
-        // Our sample is in the root of this directory, so no package name.
-        CompilationUnit cu = sourceRoot.parse("", "Blabla.java");
-
-        Log.info("Positivizing!");
-        
-        cu.accept(new ModifierVisitor<Void>() {
-
-            @Override
-            public Visitable visit(IfStmt n, Void arg) {
-                // Figure out what to get and what to cast simply by looking at the AST in a debugger! 
-                n.getCondition().ifBinaryExpr(binaryExpr -> {
-                    if (binaryExpr.getOperator() == BinaryExpr.Operator.NOT_EQUALS && n.getElseStmt().isPresent()) {
-                        Statement thenStmt = n.getThenStmt().clone();
-                        Statement elseStmt = n.getElseStmt().get().clone();
-                        n.setThenStmt(elseStmt);
-                        n.setElseStmt(thenStmt);
-                        binaryExpr.setOperator(BinaryExpr.Operator.EQUALS);
-                    }
-                });
-                return super.visit(n, arg);
-            }
-        }, null);
-
-        // This saves all the files we just read to an output directory.  
-        sourceRoot.saveAll(
-                // The path of the Maven module/project which contains the LogicPositivizer class.
-                CodeGenerationUtils.mavenModuleRoot(Main.class)
-                        // appended with a path to "output"
-                        .resolve(Paths.get("output")));
     }
 }
